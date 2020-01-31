824c8f458235bd132aca8b01e93223b680d48d48
BEAUBRUN EN FAMILLE DIANT Steven <sbeaubrundiant@gmail.com>
2020-01-31 14:35:23 +0100

    https://github.com/Sbeaubrundiant/My-Kafet-Area/issues/16 As a kitchen brigade member I want to create lunch items so that they will be available for collaborators to compose their lunch ----------------------------------------------------------------------- Created : LunchItem LunchITemService LunchItemService.Impl LunchItemController LunchItemRespository LunchItemRepositoryImpl LunchItemCreateDto Deleted all Starter references LunchItem

--- API/my-kafet-area/src/main/java/fr/formation/mykafetarea/MkaApplication.java
+++ API/my-kafet-area/src/main/java/fr/formation/mykafetarea/MkaApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import fr.formation.mykafetarea.controllers.StarterController;
-import fr.formation.mykafetarea.services.StarterService;
-import fr.formation.mykafetarea.services.impl.StarterServiceImpl;
+import fr.formation.mykafetarea.controllers.LunchItemController;
+import fr.formation.mykafetarea.services.LunchItemService;
+import fr.formation.mykafetarea.services.impl.LunchItemServiceImpl;
+
 
 @SpringBootApplication
 public class MkaApplication {
@@ -15,6 +16,8 @@ public class MkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MkaApplication.class, args);
 	}
+
+	
 	
 	
 	@Bean
@@ -22,5 +25,21 @@ public class MkaApplication {
 		ModelMapper mapper = new ModelMapper();
 		return mapper;
 	}
+	
+//	@Bean
+//	protected LunchItemServiceImpl serviceImpl () {
+//		LunchItemServiceImpl serviceImpl = new LunchItemServiceImpl(mapper, lunchItemRepository);
+//		return serviceImpl;
+//	}
+//	
+//	@Bean
+//	protected LunchItemController lunchItemController () {
+//		LunchItemController lunchItemController = new LunchItemController(lunchItemService);
+//		return lunchItemController;
+//	}
+	
+	
+
+	
 
 }
