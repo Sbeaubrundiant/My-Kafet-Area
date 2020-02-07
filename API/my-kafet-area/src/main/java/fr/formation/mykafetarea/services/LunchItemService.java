bcdd8366575142c5f27578cd5d8a92385db88969
BEAUBRUN EN FAMILLE DIANT Steven <sbeaubrundiant@gmail.com>
2020-02-08 00:13:28 +0100

    Cleannig some code smell

--- API/my-kafet-area/src/main/java/fr/formation/mykafetarea/services/LunchItemService.java
+++ API/my-kafet-area/src/main/java/fr/formation/mykafetarea/services/LunchItemService.java
@@ -1,20 +1,13 @@
 package fr.formation.mykafetarea.services;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
 import fr.formation.mykafetarea.dtos.LunchItemCreateDto;
-import fr.formation.mykafetarea.entities.LunchItem;
 
 
 public interface LunchItemService {
 	
 	/**
-	 * <p> Save lunch item to DB</p>
+	 * <p> Save lunch item to DB </p>
 	 * 
 	 *@param dto
 	 *			 Dto populate the new lunchitem with
