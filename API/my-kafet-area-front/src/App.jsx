ba656f849ddee00e46b1f64e4f01ea7e692ac972
BEAUBRUN EN FAMILLE DIANT Steven <sbeaubrundiant@gmail.com>
2020-05-14 15:37:21 +0200

    React Script update by npm

--- API/my-kafet-area-front/src/App.jsx
+++ API/my-kafet-area-front/src/App.jsx
@@ -2,8 +2,6 @@ import React, {Fragment} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppRouter from './View/router.js'
-import BootstrapTable from 'react-bootstrap-table-next';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 function App() {
